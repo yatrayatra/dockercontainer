56a5bfcc8adf5153c684c513632bdfb43bd50c85
Ubuntu <ubuntu@ip-172-31-4-174.ap-south-1.compute.internal>
2023-08-15 18:18:25 +0000

    v3

--- python-docker/app.py
+++ python-docker/app.py
@@ -1,1 +1,12 @@
-print("started")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Container started'
+
+
+if __name__ == '__main__':
+    app.run()
+
